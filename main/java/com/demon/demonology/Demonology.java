4826a9762a7b065dd70009802cab627cf3521ae3
spencerflem <spencerflem@gmail.com>
2015-08-22 00:32:06 -0400

    Finished boilerplate
    Ready for the real mods to roll in

--- main/java/com/demon/demonology/Demonology.java
+++ main/java/com/demon/demonology/Demonology.java
@@ -1,10 +1,32 @@
 package com.demon.demonology;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Demonology.MODID, version = Demonology.VERSION)
+@Mod(modid = "demonology", name="Demonology", version = "1.7.10")
 public class Demonology 
 {
-	public static final String MODID = "demonology";
-	public static final String VERSION = "0.0.1";
+	@Mod.Instance("Demonology")
+	public static Demonology instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) 
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) 
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) 
+	{
+		
+	}
 }
