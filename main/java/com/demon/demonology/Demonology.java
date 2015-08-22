0d3346e767c37165f0658015b0ee21342e211827
spencerflem <spencerflem@gmail.com>
2015-08-21 23:13:07 -0400

    Added Java Files
    All Here

---
+++ main/java/com/demon/demonology/Demonology.java
@@ -0,0 +1,10 @@
+package com.demon.demonology;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Demonology.MODID, version = Demonology.VERSION)
+public class Demonology 
+{
+	public static final String MODID = "demonology";
+	public static final String VERSION = "0.0.1";
+}
